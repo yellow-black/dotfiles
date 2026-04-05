c49ed3d94221a4699e9c4107678bfbd838ebd0f7
s.t <19802528+yellow-black@users.noreply.github.com>
2026-04-05 21:51:11 +0900

    Insertモード終了時に自動で日本語入力をオフに

--- .chezmoiscripts/run_once_02_setup-nvim.sh
+++ .chezmoiscripts/run_once_02_setup-nvim.sh
@@ -4,6 +4,11 @@ sudo add-apt-repository -y ppa:neovim-ppa/unstable
 sudo apt update
 sudo apt install -y neovim
 
+# IME 自動切換え
+curl -LO https://github.com/kyoh86/zenhan/releases/download/v0.0.1/zenhan.exe
+sudo mv zenhan.exe ~/.local/bin/zenhan.exe
+sudo chmod +x ~/.local/bin/zenhan.exe
+
 # dot_config/nvim/ で既に管理されているため、以下の操作は不要
 # # https://www.lazyvim.org/installation
 
