c44a6cc583c77a442b67c6d4081ea1c020f4b8dc
s.t <19802528+yellow-black@users.noreply.github.com>
2026-04-08 08:25:07 +0900

    use abbr

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,6 +1,23 @@
 set -gx EDITOR code
 set -gx VISUAL code
 
+abbr -a v nvim
+abbr -a vi nvim
+abbr -a vim nvim
+
+abbr -a py python
+abbr -a ipy ipython
+
+abbr -a g git
+abbr -a gs git status
+abbr -a ga git add
+abbr -a gc git commit
+abbr -a gsw git switch
+abbr -a gb git branch
+abbr -a gp git push
+abbr -a gl 'git log --oneline --graph --decorate'
+abbr -a gd git diff
+
 alias fd="fdfind"
 alias ls="eza --icons"
 alias cat="batcat"
