cd483e144dc6f0685541a59620483df1c5bfbaaa
s.t <19802528+yellow-black@users.noreply.github.com>
2026-04-10 02:47:41 +0900

    VSCode 向けの設定を追加

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -19,19 +19,38 @@ map("i", "<C-k>", "<Up>", opts)
 map("i", "<C-l>", "<Right>", opts)
 
 if vim.g.vscode then
-  map("n", "<leader>e", function()
-    require("vscode").action("workbench.view.explorer")
-  end, { desc = "Open VSCode Explorer" })
+  -- map("n", "<leader>e", function()
+  --   require("vscode").action("workbench.view.explorer")
+  -- end, { desc = "Open VSCode Explorer" })
 
-  map("n", "<leader>f", function()
-    require("vscode").action("workbench.action.findInFiles")
-  end, { desc = "Find in Files" })
+  -- map("n", "<leader>f", function()
+  --   require("vscode").action("workbench.action.findInFiles")
+  -- end, { desc = "Find in Files" })
 
-  map("n", "<leader>g", function()
-    require("vscode").action("workbench.view.scm")
-  end, { desc = "Show VSCode Source Control" })
+  map("n", "<S-h>", function()
+    require("vscode").action("workbench.action.previousEditor")
+  end, { desc = "Navigate to Previous VSCode Editor" })
 
-  map("n", "<leader>x", function()
-    require("vscode").action("workbench.view.extensions")
-  end, { desc = "Show VSCode Extensions" })
+  map("n", "<S-l>", function()
+    require("vscode").action("workbench.action.nextEditor")
+  end, { desc = "Navigate to Next VSCode Editor" })
+
+  pcall(vim.keymap.del, "n", "<leader>uZ")
+  map("n", "<leader>uz", function()
+    require("vscode").action("workbench.action.toggleZenMode")
+  end, { desc = "Toggle VSCode Zen Mode" })
+
+  pcall(vim.keymap.del, "n", "<leader>gg")
+  pcall(vim.keymap.del, "n", "<leader>gG")
+  map("n", "<leader>gg", function()
+    require("vscode").action("lazygit-vscode.toggle")
+  end, { desc = "Toggle VSCode LazyGit" })
+
+  -- Git周りのキーマップは無意味なので削除
+  local keys = { "<leader>gY", "<leader>gB", "<leader>gl", "<leader>gL", "<leader>gf", "<leader>gp", "<leader>gP",
+    "<leader>gi", "<leader>gI", "<leader>gd", "<leader>gD", "<leader>gs", "<leader>gS" }
+  for _, key in ipairs(keys) do
+    pcall(vim.keymap.del, "n", key)
+    pcall(vim.keymap.del, "x", key)
+  end
 end
