8d004b34af9431e7f8826b15ef7f97ea47c24338
s.t <19802528+yellow-black@users.noreply.github.com>
2026-04-12 20:52:16 +0900

    timeout を長めに

--- dot_config/private_fish/functions/nordvpn_status.fish
+++ dot_config/private_fish/functions/nordvpn_status.fish
@@ -1,6 +1,6 @@
 function nordvpn_status
     if test -d "/mnt/c/Program Files/NordVPN"
-        set -l protected (curl -s --connect-timeout 0.3 --max-time 1 "https://web-api.nordvpn.com/v1/ips/info" | jq '. | .protected')
+        set -l protected (curl -s --connect-timeout 0.5 --max-time 1 "https://web-api.nordvpn.com/v1/ips/info" | jq '. | .protected')
         if test "$protected" = true
             echo "NordVPN is currently active and protecting your connection."
             return 0
