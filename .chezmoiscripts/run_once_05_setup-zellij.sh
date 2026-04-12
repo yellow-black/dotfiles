03dfa65a374be50bdb25ae1023254705cce4585a
s.t <19802528+yellow-black@users.noreply.github.com>
2026-04-13 03:05:12 +0900

    zellij が既に存在する場合に skip するように

--- .chezmoiscripts/run_once_05_setup-zellij.sh
+++ .chezmoiscripts/run_once_05_setup-zellij.sh
@@ -3,6 +3,13 @@
 # 左上の " Zellij "(Session Name) -> " "(Session Name) に変更
 # quit 時の "Bye from Zellij!" -> " " に変更
 
+if ! command -v zellij &>/dev/null; then
+  echo "Zellij is not installed. Installing..."
+else
+  echo "Zellij is already installed. Skipping installation."
+  exit 0
+fi
+
 sudo apt install -y protobuf-compiler
 
 mkdir -p ~/src
