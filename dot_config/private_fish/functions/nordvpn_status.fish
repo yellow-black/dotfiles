b0e28c93ec524b40ff69f412e611111eb36c0245
s.t <19802528+yellow-black@users.noreply.github.com>
2026-04-12 19:50:22 +0900

    nordvpn の状況をチェックするスクリプトの作成

---
+++ dot_config/private_fish/functions/nordvpn_status.fish
@@ -0,0 +1,15 @@
+function nordvpn_status
+    if test -d "/mnt/c/Program Files/NordVPN"
+        set -l protected (curl -s --connect-timeout 0.1 --max-time 1 "https://web-api.nordvpn.com/v1/ips/info" | jq '. | .protected')
+        if test "$protected" = true
+            echo "NordVPN is currently active and protecting your connection."
+            return 0
+        else
+            echo "NordVPN is installed but not currently active."
+            return 1
+        end
+    else
+        echo "NordVPN is not installed on this system."
+        return 1
+    end
+end
